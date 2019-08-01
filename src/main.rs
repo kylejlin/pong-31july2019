bd3319e6c7d8c42293fe9ce5202174df49b1f361
Kyle Lin <kylejlin.us@gmail.com>
2019-08-01 12:39:21 -0700

    4.3

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 //! Pong Tutorial 1
 
 mod pong;
+mod systems;
 
 use crate::pong::Pong;
 
 use amethyst::core::transform::TransformBundle;
+use amethyst::input::{InputBundle, StringBindings};
 use amethyst::{
     prelude::*,
     renderer::{
@@ -21,6 +23,11 @@ fn main() -> amethyst::Result<()> {
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
 
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle =
+        InputBundle::<StringBindings>::new().with_bindings_from_file(binding_path)?;
+
     let game_data = GameDataBuilder::default()
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
@@ -32,7 +39,9 @@ fn main() -> amethyst::Result<()> {
                 // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
                 .with_plugin(RenderFlat2D::default()),
         )?
-        .with_bundle(TransformBundle::new())?;
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"]);
 
     let assets_dir = app_root.join("assets");
     let mut game = Application::new(assets_dir, Pong, game_data)?;
--- src/pong.rs
+++ src/pong.rs
@@ -21,8 +21,6 @@ impl SimpleState for Pong {
         // Load the spritesheet necessary to render the graphics.
         let sprite_sheet_handle = load_sprite_sheet(world);
 
-        world.register::<Paddle>();
-
         initialise_paddles(world, sprite_sheet_handle);
         initialise_camera(world);
     }
